5d27d223c558869e321efcbe768223e375f6413b
Rory Powell <rory.codes@gmail.com>
2022-05-05 13:50:55 +0100

    wip

--- packages/server/src/api/controllers/backup.js
+++ packages/server/src/api/controllers/backup.js
@@ -1,4 +1,5 @@
 const { streamBackup } = require("../../utilities/fileSystem")
+const { objectStore } = require("@budibase/backend-core")
 
 exports.exportAppDump = async function (ctx) {
   const { appId } = ctx.query
@@ -6,4 +7,10 @@ exports.exportAppDump = async function (ctx) {
   const backupIdentifier = `${appName}-export-${new Date().getTime()}.txt`
   ctx.attachment(backupIdentifier)
   ctx.body = await streamBackup(appId)
+
+  const test = await objectStore.retrieve(
+    "prod-budi-app-assets",
+    "app_default_d5cfa5eba37c4ce8af11bb94d6d6e904/attachments/movie.mov"
+  )
+  console.log(test)
 }
